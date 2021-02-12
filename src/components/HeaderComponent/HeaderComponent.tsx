5563a2a969d864776c8bcf9dacc98db9605e6b3b
KRISTINE ANN AVENIDO <kristineannavenido@KRISTINEs-MacBook-Air.local>
2021-02-12 18:10:27 +0800

    initial commit

---
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './HeaderComponent.module.scss';
+
+const HeaderComponent: React.FC = () => (
+  <div className={styles.HeaderComponent}>
+    HeaderComponent Component
+  </div>
+);
+
+export default HeaderComponent;
